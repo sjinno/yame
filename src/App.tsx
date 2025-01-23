b62fa5b2fd3c14947b9ac14a8597db016907ea17
Shohei Jinno <shohei.jinno@gmail.com>
2025-01-23 11:27:27 -0800

    feat: integrate tauri-plugin-store for persistent data storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect, useState } from 'react';
 import { Timer } from './components';
+import './App.css';
+import { store } from './store';
 
 function App() {
   const [timers, setTimers] = useState([1]);
+
+  useEffect(() => {
+    (async () => {
+      const world = await store?.get('hello');
+      console.log('hello', world);
+    })();
+  }, []);
 
   function handleAddTimer() {
     const id = timers.length + 1;
