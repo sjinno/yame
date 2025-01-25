fb89964151524a1eee3bde7a5d3e5fa34538a693
Shohei Jinno <shohei.jinno@gmail.com>
2025-01-24 20:51:06 -0800

    fix: handle division by zero

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -25,7 +25,9 @@ function calcProgress(hms: Hms, originalHms: Hms): number {
   const m2 = originalHms.minutes ?? 0;
   const s2 = originalHms.seconds ?? 0;
 
-  const total = h2 * 60 * 60 + m2 * 60 + s2;
+  const totalSeconds = h2 * 60 * 60 + m2 * 60 + s2;
 
-  return (((h2 - h1) * 60 * 60 + (m2 - m1) * 60 + (s2 - s1)) / total) * 100;
+  return totalSeconds === 0
+    ? 0
+    : (((h2 - h1) * 60 * 60 + (m2 - m1) * 60 + (s2 - s1)) / totalSeconds) * 100;
 }
